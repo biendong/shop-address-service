083e3f058e26d6e5b98329a142e6441920de9f89
Hai Tran Dong <hai.trandong@efundamentals.com>
2023-10-03 23:28:21 +0700

    Initiate

---
+++ src/test/java/com/shop/address/AddressServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.address;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
